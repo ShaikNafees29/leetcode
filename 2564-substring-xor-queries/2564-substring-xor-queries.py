3b0dbf944861bd7cf963344f9e8358964009d37b
Shaik Nafees Ahamad <shaiknafees2904@gmail.com>
2026-03-28 10:53:35 +0530

    Time: 79 ms (90.79%), Space: 78.6 MB (71.05%) - LeetHub

---
+++ 2564-substring-xor-queries/2564-substring-xor-queries.py
@@ -0,0 +1,31 @@
+class Solution:
+    def substringXorQueries(self, s: str, queries: list[list[int]]) -> list[list[int]]:
+        # 1. Pre-calculate all possible decimal values in the string
+        # We only need substrings up to 30 bits long.
+        occurences = {}
+        n = len(s)
+        
+        # Traverse the string once to map each value to its [start, end]
+        # We go from left to right so we always store the FIRST occurrence.
+        for i in range(n):
+            if s[i] == '0':
+                if 0 not in occurences:
+                    occurences[0] = [i, i]
+                continue # Leading zeros only matter for the value 0
+            
+            val = 0
+            for j in range(i, min(i + 30, n)):
+                val = (val << 1) + int(s[j])
+                if val not in occurences:
+                    occurences[val] = [i, j]
+        
+        # 2. Process queries using the pre-calculated map
+        res = []
+        for i, j in queries:
+            target = i ^ j
+            if target in occurences:
+                res.append(occurences[target])
+            else:
+                res.append([-1, -1])
+                
+        return res
