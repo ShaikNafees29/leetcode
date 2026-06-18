c2983aeebbe7d494ac99272f74a02cebdbe60da7
Shaik Nafees Ahamad <shaiknafees2904@gmail.com>
2026-06-18 09:58:46 +0530

    Time: 4 ms (99.7%), Space: 48.1 MB (11.81%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        for(int i=0,j=s.length()-1;i<j;i++,j--){
+            if(s.charAt(i)!=s.charAt(j)){
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s,int l,int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
