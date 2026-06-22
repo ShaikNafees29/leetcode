3d2e381c073d152539e450de4ac0bb1e5fabd9e0
Shaik Nafees Ahamad <shaiknafees2904@gmail.com>
2026-06-22 09:38:27 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.73%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int f=0,l=nums.length-1;
+        while(f<=l){
+            int mid=(f+l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                f=mid+1;
+            }
+            else{
+                l = mid-1;
+            }
+        }
+        return -1;
+    }
+}
